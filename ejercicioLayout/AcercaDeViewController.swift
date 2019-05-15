62ee9b8226a729f397ce2d7f7b0cef1b3b676b22
Alber <34082417+AN0NIM0U5@users.noreply.github.com>
2019-05-15 17:32:50 +0200

    Add files via upload

---
+++ ejercicioLayout/AcercaDeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AcercaDeViewController.swift
+//  ejercicioLayout
+//
+//  Created by Dev2 on 14/05/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class AcercaDeViewController: UIViewController {
+
+//    @IBOutlet weak var txtAcercaDe: UITextField!
+//
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//            txtAcercaDe.text = "Acerca de"
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ ejercicioLayout/LegalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LegalViewController.swift
+//  ejercicioLayout
+//
+//  Created by Dev2 on 14/05/2019.
+//  Copyright © 2019 Dev2. All rights reserved.
+//
+
+import UIKit
+
+class LegalViewController: UIViewController {
+
+//    @IBOutlet weak var txtLegal: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        txtLegal.text = "patata frita"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
